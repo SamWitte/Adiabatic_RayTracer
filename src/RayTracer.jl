2ba6d602b288e57477cd648f2b5faa131df75331
Samuel Witte <witte.sam@gmail.com>
2023-06-09 15:40:27 +0200

    if on bndry lay

--- src/RayTracer.jl
+++ src/RayTracer.jl
@@ -785,16 +785,17 @@ function GJ_Model_vec(x, t, θm, ω, B0, rNS; bndry_lyr=-1)
     ωp = sqrt.(4 .* π .* nelec ./ 137 ./ 5.0e5);
 
     # format: [e-, e+] last two -- plasma mass and gamma factor
-    
-    if bndry_lyr > 0.0
-        nelec_pole = abs.((2.0 .* ω .* B0) ./ sqrt.(4 .* π ./ 137) .* (1.95e-2) .* hbar) ; # eV^3
-        pole_val = sqrt.(4 .* π .* nelec_pole ./ 137 ./ 5.0e5);
-        if bndry_lyr == (1.5)
-            ωp[r .>= rNS] = pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
-        else
-            ωp[r .>= rNS] .+= pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+    if sum(r .>= rNS) > 0
+        if bndry_lyr > 0.0
+            nelec_pole = abs.((2.0 .* ω .* B0) ./ sqrt.(4 .* π ./ 137) .* (1.95e-2) .* hbar) ; # eV^3
+            pole_val = sqrt.(4 .* π .* nelec_pole ./ 137 ./ 5.0e5);
+            if bndry_lyr == (1.5)
+                ωp[r .>= rNS] = pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+            else
+                ωp[r .>= rNS] .+= pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+            end
+                
         end
-            
     end
     
     return [Bx By Bz], ωp
@@ -1005,14 +1006,16 @@ function GJ_Model_ωp_vec(x, t, θm, ω, B0, rNS; bndry_lyr=-1)
     nelec = abs.((2.0 .* ω .* Bz) ./ sqrt.(4 .* π ./ 137) .* (1.95e-2) .* hbar) ; # eV^3
     ωp = sqrt.(4 .* π .* nelec ./ 137 ./ 5.0e5);
 
-    if bndry_lyr > 0
-        nelec_pole = abs.((2.0 .* ω .* B0) ./ sqrt.(4 .* π ./ 137) .* (1.95e-2) .* hbar) ; # eV^3
-        pole_val = sqrt.(4 .* π .* nelec_pole ./ 137 ./ 5.0e5);
-        
-        if bndry_lyr == (1.5)
-            ωp[r .>= rNS] = pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
-        else
-            ωp[r .>= rNS] .+= pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+    if sum(r .>= rNS) > 0
+        if bndry_lyr > 0
+            nelec_pole = abs.((2.0 .* ω .* B0) ./ sqrt.(4 .* π ./ 137) .* (1.95e-2) .* hbar) ; # eV^3
+            pole_val = sqrt.(4 .* π .* nelec_pole ./ 137 ./ 5.0e5);
+            
+            if bndry_lyr == (1.5)
+                ωp[r .>= rNS] = pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+            else
+                ωp[r .>= rNS] .+= pole_val[r .>= rNS] .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+            end
         end
     end
     
@@ -1069,14 +1072,15 @@ function GJ_Model_ωp_vecSPH(x, t, θm, ω, B0, rNS; zeroIn=true, bndry_lyr=-1)
     
     nelec = abs.((2.0 .* ω .* Bz) ./ sqrt.(4 .* π ./ 137) .* 1.95e-2 .* hbar) ; # eV^3
     ωp = sqrt.(4 .* π .* nelec ./ 137 ./ 5.0e5);
-    
-    if bndry_lyr > 0
-        nelec_pole = abs.((2.0 .* ω .* B0) ./ sqrt.(4 .* π ./ 137) .* (1.95e-2) .* hbar) ; # eV^3
-        pole_val = sqrt.(4 .* π .* nelec_pole ./ 137 ./ 5.0e5);
-        if bndry_lyr == (1.5)
-            ωp[r .>= rNS] = pole_val .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
-        else
-            ωp[r .>= rNS] .+= pole_val .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+    if sum(r .>= rNS) > 0
+        if bndry_lyr > 0
+            nelec_pole = abs.((2.0 .* ω .* B0) ./ sqrt.(4 .* π ./ 137) .* (1.95e-2) .* hbar) ; # eV^3
+            pole_val = sqrt.(4 .* π .* nelec_pole ./ 137 ./ 5.0e5);
+            if bndry_lyr == (1.5)
+                ωp[r .>= rNS] = pole_val .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+            else
+                ωp[r .>= rNS] .+= pole_val .* (rNS ./ r[r .>= rNS]).^(bndry_lyr)
+            end
         end
     end
 
